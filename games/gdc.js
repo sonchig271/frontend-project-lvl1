5222bd117df8ccdeb4fccd8a84f7cbba754aa3cb
Sonya Chigireva <sonchig314@gmail.com>
2022-01-23 21:03:08 +0200

    fix gdc game

--- games/gdc.js
+++ games/gdc.js
@@ -1,7 +1,6 @@
-import readlineSync from 'readline-sync';
 import game from '../src/index.js';
 
-const getRandomInt = () => Math.floor(1 + Math.random() * 100);
+const getRandomInt = () => Math.floor(1 + Math.random() * 20);
 const getGCD = (num1, num2) => {
   if (num2 === 0) {
     return num1;
@@ -9,20 +8,18 @@ const getGCD = (num1, num2) => {
   return getGCD(num2, num1 % num2);
 };
 const gameGCD = () => {
-  const name = readlineSync.question('What is your name?: ');
-  console.log(`Hello, ${name}`);
   const rule = 'Find the greatest common divisor of given numbers.';
-  let counter = 0;
-  let result = true;
-  while (result && counter < 3) {
-    counter += 1;
+  const questions = [];
+  const rightAnswers = [];
+  for (let i = 0; i < 3; i += 1) {
     const firstNumber = getRandomInt();
     const secondNumber = getRandomInt();
     const question = `${firstNumber} ${secondNumber}`;
     const rightAnswer = getGCD(firstNumber, secondNumber);
-    result = game(rule, question, rightAnswer);
+    questions.push(question);
+    rightAnswers.push(rightAnswer);
   }
-  if (counter === 3) console.log('Congratulations!');
+  game(rule, questions, rightAnswers);
 };
 
 export default gameGCD;
