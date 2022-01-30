225e589b4b6c33f65dabf88d852e3b4e8a0729d2
Sonya Chigireva <sonchig314@gmail.com>
2022-01-30 11:59:57 +0200

    add getMissingTerm function

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,24 +6,25 @@ const rule = 'What number is missing in the progression?';
 const getProgression = (start, step, missedIndex) => {
   const progression = [start];
   let num = start;
-  let missed;
   for (let i = 0; i < 9; i += 1) {
     num += step;
     if (i === missedIndex) {
       progression.push('..');
-      missed = num;
     } else {
       progression.push(num);
     }
   }
-  return [progression.join(' '), missed];
+  return progression.join(' ');
 };
+const getMissingTerm = (start, step, index) => start + step * (index);
+
 const gameProgression = () => {
   const questions = [];
   const rightAnswers = [];
   for (let i = 0; i < 3; i += 1) {
     const index = getRandomInt(1, 8);
-    const [question, rightAnswer] = getProgression(getRandomInt(1, 20), getRandomInt(2, 9), index);
+    const question = getProgression(getRandomInt(1, 20), getRandomInt(2, 9), index);
+    const rightAnswer = getMissingTerm(getRandomInt(1, 20), getRandomInt(2, 9), index);
     questions.push(question);
     rightAnswers.push(rightAnswer);
   }
